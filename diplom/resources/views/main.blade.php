d5d17a15c83b8f0e51d0074276ddf32a4587f3f2
Kirill Grushkevich <lukedarling@mail.ru>
2022-11-02 11:46:43 +0300

    laravel1.0

---
+++ diplom/resources/views/main.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="b-header otstup">
+		<div class="header-logo">
+			<a href="#">
+				Мир алкоголя
+			</a>
+		</div>
+		<div class="header-menu">
+			<ul class="header-menu-list">
+				<li class="header-menu-item">
+					<a href="#">Вся продукция</a>
+				</li>
+				<li class="header-menu-item">
+					<a href="#">Акции</a>
+				</li>
+				<li class="header-menu-item">
+					<a href="#">Новости</a>
+				</li>
+				<li class="header-menu-item">
+					<a href="#">О нас</a>
+				</li>
+				<li class="header-menu-item">
+					<a href="#">Поддержка</a>
+				</li>
+			</ul>
+		</div>
+</body>
+</html>
